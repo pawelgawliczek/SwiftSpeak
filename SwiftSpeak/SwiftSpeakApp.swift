b781785a57276aada6d2a5dc0bffe5a9299d0356
pawelgawliczek <gawliczek.pawel@gmail.com>
2025-12-26 06:55:28 +0200

    Initial Commit

---
+++ SwiftSpeak/SwiftSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSpeakApp.swift
+//  SwiftSpeak
+//
+//  Created by Pawel Gawliczek on 26/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
